020b7cdd95da18aab2803d891c0795f1f73f5578
carlosduram71 <jorgemiranda0180@gmail.com>
2021-12-27 20:40:23 -0400

    forthq

--- src/components/notifications.py/Notification.js
+++ src/components/notifications.py/Notification.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types'
 const Notification = ({auth:{user,loading}}) => {
   //Public API that will echo messages sent to it back to the client
 
-  const initialValue = JSON.parse(localStorage.getItem("notifications")|| "[]");
+  //const initialValue = JSON.parse(localStorage.getItem("notifications")|| "[]");
   
     //document.querySelector('#chat-log').value += (data.message + '\n');
 
@@ -17,13 +17,7 @@ const Notification = ({auth:{user,loading}}) => {
           Your cart is empty {}<Link to='/'>Go Back</Link>
           </div>
           ) : (
-            <div className='bg-gray-400 mx-auto h-screen w-scrren mt-20'>
-            {initialValue.map((item)=>(
-              <div>{item.notifications}100
-          <img className='h-32 w-20' src={item.image} alt="" />
-          </div>
-        ))}sasas
-        </div>
+            <div className='bg-gray-400 mx-auto h-screen w-scrren mt-20'>        </div>
         )}
       
         </Fragment>
